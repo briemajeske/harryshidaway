ab03ac3dbe06cafea6e4ca9b2fc2555df77433f8
briemajeske <bmajeske@capellauniversity.edu>
2021-09-16 21:28:40 -0700

    javascript magic

--- form.js
+++ form.js
@@ -6,7 +6,9 @@ window.addEventListener('load', function () {
 const handleSubmit = (e) => {
     e.preventDefault();
   let myForm = document.getElementById('contactForm');
-  let formData = new FormData(myForm)
+  let formData = new FormData(myForm);
+  let firstname = formData.get("firstname");
+
   fetch('/', {
     method: 'POST',
     headers: { "Content-Type": "multipart/form-data" },
@@ -14,16 +16,16 @@ const handleSubmit = (e) => {
   }).then((res) => {
     console.log('Form successfully submitted')
     console.log("response is", res);
-    formSubmittedSuccess(e);
+    formSubmittedSuccess(firstname);
 }).catch((error) =>
     alert("There was a problem submitting your form"));
 }
 
-const formSubmittedSuccess = (e) => {
+const formSubmittedSuccess = (firstname) => {
     let myForm = document.getElementById('contactForm');
     myForm.reset();
     let successMsg = document.createElement("p");
-    let msg = document.createTextNode("Thanks for submitting feedback!");
+    let msg = document.createTextNode(`Thanks for submitting feedback, ${firstname}!`);
     successMsg.append(msg);
     successMsg.setAttribute("class", "thankyou-message");
     myForm.appendChild(successMsg);
